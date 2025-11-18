88cc9044da5111fce94667082fa7513e721ca665
ArielSanchez12 <riveraariel433@gmail.com>
2025-11-18 13:55:09 -0500

    commit 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+   supabase: {
+    url: 'https://uydswruhckrywwrdzhrg.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5ZHN3cnVoY2tyeXd3cmR6aHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0ODgxOTQsImV4cCI6MjA3OTA2NDE5NH0.4AFmEsK4a9TGoANpmsj-FCZ9UoeGKDs2VXYGphetYlM'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabase: {
+    url: 'https://uydswruhckrywwrdzhrg.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5ZHN3cnVoY2tyeXd3cmR6aHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0ODgxOTQsImV4cCI6MjA3OTA2NDE5NH0.4AFmEsK4a9TGoANpmsj-FCZ9UoeGKDs2VXYGphetYlM'
+  }
 };
 
 /*
